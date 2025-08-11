49991f7c572712ae07dae8f6e7fc97c83ec905e1
cat <114403771+makaseloli@users.noreply.github.com>
2025-08-11 22:40:01 +0900

    歴史を修正

--- src/main/java/io/github/meatwo310/tsukichat/config/CommonConfig.java
+++
@@ -1,9 +0,0 @@
-package io.github.meatwo310.tsukichat.config;
-
-import net.neoforged.neoforge.common.ModConfigSpec;
-
-public class CommonConfig {
-    private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
-
-    public static final ModConfigSpec SPEC = BUILDER.build();
-}
