032b2be71efbbac99bab7726235fa9e53b40b8e8
Meatwo310 <72017364+Meatwo310@users.noreply.github.com>
2025-08-13 18:14:29 +0900

    Initial commit

---
+++ src/main/java/io/github/meatwo310/examplemod/ExampleMod.java
@@ -0,0 +1,12 @@
+package io.github.meatwo310.examplemod;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(ExampleMod.MODID)
+public class ExampleMod {
+    public static final String MODID = "examplemod";
+
+    public ExampleMod(FMLJavaModLoadingContext context) {
+    }
+}
