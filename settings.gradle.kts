7b8b28514adfc9c53e9d1d6c6d854523bc7742af
Meatwo310 <git@meatwo310.net>
2026-06-03 15:26:11 +0900

    chore: 不要なサブプロジェクトを無効化

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,39 +17,39 @@ plugins {
 include("common")
 
 /// 1.18.2 ///
-include("1.18.2-common")
-include("1.18.2-forge")
+// include("1.18.2-common")
+// include("1.18.2-forge")
 
 /// 1.19.2 ///
-include("1.19.2-common")
-include("1.19.2-forge")
+// include("1.19.2-common")
+// include("1.19.2-forge")
 
 /// 1.20.1 ///
 include("1.20.1-common")
 include("1.20.1-forge")
-include("1.20.1-fabric")
+// include("1.20.1-fabric")
 
 /// 1.21.1 ///
-include("1.21.1-common")
-include("1.21.1-neo")
-include("1.21.1-fabric")
+// include("1.21.1-common")
+// include("1.21.1-neo")
+// include("1.21.1-fabric")
 
 /// 1.21.8 ///
-include("1.21.8-common")
-include("1.21.8-fabric")
+// include("1.21.8-common")
+// include("1.21.8-fabric")
 
 /// 1.21.11 ///
-include("1.21.11-common")
-include("1.21.11-fabric")
+// include("1.21.11-common")
+// include("1.21.11-fabric")
 
 /// 26.1 ///
-include("26.1-common")
-include("26.1-fabric")
-include("26.1-neo")
+// include("26.1-common")
+// include("26.1-fabric")
+// include("26.1-neo")
 
 /// 26.1.2 ///
 include("26.1.2-common")
-include("26.1.2-fabric")
+// include("26.1.2-fabric")
 include("26.1.2-neo")
 
 val ciBuildProjectNames = rootProject.children
