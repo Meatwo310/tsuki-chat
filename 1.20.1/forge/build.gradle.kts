2960bd9c245d3e5a6f8f04be9d67e3ae1bb46c90
Meatwo310 <git@meatwo310.net>
2026-07-13 13:54:23 +0900

    build(1.20.1-forge): bump integration mods

--- 1.20.1/forge/build.gradle.kts
+++ 1.20.1/forge/build.gradle.kts
@@ -6,9 +6,9 @@ val configuredVersion: String by project
 
 // Mod Dependencies
 dependencies {
-    modImplementation("curse.maven:craterlib-867099:5574012")
-    modImplementation("curse.maven:simple-discord-link-bot-forge-fabric-spigot-541320:5574099")
+    modImplementation("maven.modrinth:craterlib:1.20-3.1.2+hotfix.1")
+    modImplementation("maven.modrinth:sdlink:3.4.3")
 
     modRuntimeOnly(libs.configured) { version { require(configuredVersion) } }
-    modRuntimeOnly("curse.maven:modern-ui-352491:6199942")
+    modRuntimeOnly("curse.maven:modern-ui-352491:6956345")
 }
--- 1.20.1/forge/src/main/java/net/meatwo310/tsukichat/compat/SDLinkServerChatEvent.java
+++ 1.20.1/forge/src/main/java/net/meatwo310/tsukichat/compat/SDLinkServerChatEvent.java
@@ -1,28 +1,26 @@
 package net.meatwo310.tsukichat.compat;
 
-import com.hypherionmc.craterlib.nojang.world.entity.player.BridgedPlayer;
+import com.hypherionmc.craterlib.api.game.text.Text;
+import com.hypherionmc.craterlib.impl.api.world.entity.player.BridgedPlayer;
 import com.hypherionmc.sdlink.core.managers.HiddenPlayersManager;
 import com.hypherionmc.sdlink.platform.SDLinkMCPlatform;
 import com.hypherionmc.sdlink.server.ServerEvents;
 import net.meatwo310.tsukichat.config.CommonConfigs;
-import net.minecraft.network.chat.Component;
 import net.minecraftforge.event.ServerChatEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import shadow.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
 
 public class SDLinkServerChatEvent {
     @SubscribeEvent
     public void onServerChat(ServerChatEvent event) {
         if (!CommonConfigs.sdlinkCompat.get()) return;
 
-        BridgedPlayer player = BridgedPlayer.of(event.getPlayer());
-        String messageString = Component.Serializer.toJson(event.getMessage());
-        shadow.kyori.adventure.text.Component adventureComponent = GsonComponentSerializer.gson().deserialize(messageString);
+        BridgedPlayer player = BridgedPlayer.wrap(event.getPlayer());
+        Text message = Text.fromGame(event.getMessage());
 
         if (SDLinkMCPlatform.INSTANCE.playerIsActive(player)) {
             if (!HiddenPlayersManager.INSTANCE.isPlayerHidden(event.getPlayer().getStringUUID())) {
                 ServerEvents.getInstance().onServerChatEvent(
-                        adventureComponent,
+                        message,
                         player.getDisplayName(),
                         SDLinkMCPlatform.INSTANCE.getPlayerSkinUUID(player),
                         player.getGameProfile(),
